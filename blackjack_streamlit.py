8ee5509ec14b7cf85e53c7552e0f20b1c5e1f5ea
Kevin Stone <kstone40@gatech.edu>
2021-10-31 16:31:10 -0400

    Bug fix

--- blackjack_streamlit.py
+++ blackjack_streamlit.py
@@ -87,11 +87,11 @@ options['player_names'] = []
 player_strats = st.sidebar.expander('Player Strategies')
 with player_strats:
     for p_ in range(options['player_count']):
-        st.subheader(f"Player {p_}")
+        st.subheader(f"Player {p_+1}")
         custom_strat = player_strats.checkbox('Custom?', value=0, key=str(p_)+'strat_custom',
                                           help ='Check if you want to upload a custom strategy')
         options['player_strat'].append({})
-        options['player_names'].append(player_strats.text_input('Name', value=f"{options['player_names'][p_]}", key=str(p_)+'name'))
+        options['player_names'].append(player_strats.text_input('Name', value=f"Player [p_+1]}", key=str(p_)+'name'))
         if custom_strat:
             #Show example file
             data = open('Strategies/strategy_custom_example.xlsx', 'rb').read()
