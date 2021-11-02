9d8ac5d77cc48701b657ca54f135db50b7001303
kstone40 <91576158+kstone40@users.noreply.github.com>
2021-11-02 11:30:18 -0400

    Update blackjack_streamlit.py

--- blackjack_streamlit.py
+++ blackjack_streamlit.py
@@ -108,11 +108,12 @@ with player_strats:
             st.markdown(href, unsafe_allow_html=True)
             uploaded_file = player_strats.file_uploader('Choose a file', key=str(p_))
             if uploaded_file is not None:
-                options['player_strat'][p_]['Special'] = 'Custom'
-                options['player_strat'][p_]['Path'] = uploaded_file.name
+                options['player_strat'][p_]['Custom'] = uploaded_file.name
             if uploaded_file is None:
-                options['player_strat'][p_]['Special'] = 'Custom'
-                options['player_strat'][p_]['Path'] = 'Strategies/strategy_custom_example.xlsx'
+                options['player_strat'][p_]['Custom'] = 'Strategies/strategy_custom_example.xlsx'
+            options['player_strat'][p_]['Hard'] = 1
+            options['player_strat'][p_]['Soft'] = 1
+            options['player_strat'][p_]['Split'] = 1  
         else:        
             choices = ['None/Dealer','Optimal']
             hard_strat = player_strats.selectbox('Hard-Total Strategy',choices,index=1,help='Select a strategy to test',key=str(p_)+'strat_hard')
